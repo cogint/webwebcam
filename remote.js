6803ee1f7bbdde8ed9335bce3542985eeecab3d4
Chad Hart <chad@cwh.consulting>
2021-05-07 09:33:16 -0400

    remote redo

--- remote.js
+++ remote.js
@@ -2,21 +2,18 @@ import {mungeH264} from "./extension/modules/mungeH264.mjs";
 
 let video = document.querySelector('video');
 let changeCam = document.getElementById('changeCam');
-// let openCamBtn = document.getElementById('scanQr');
-let permissions = document.getElementById('scanPermission');
 let status = document.getElementById('status');
+let clickForCam =  document.getElementById('clickForCam');
 
-// let mobile = false;
-// let facingMode = "user";
 const CALL_RETRY_PERIOD = 2 * 1000;
 let deviceIds = [];
-let index = -1;
+let index = 1;
 let currentDeviceId = "";
 let disablePeer = false;
 
 let peer, extCall, pageCall; // Global holders for calls
+let peerId = false;
 let stream = null; // make this global for debugging
-
 
 function errorHandler(error) {
     console.error(error)
@@ -25,37 +22,24 @@ function errorHandler(error) {
     // connExt.send(`${e.name}: ${e.message}`);
 }
 
-// Scans all video devices to see if facing mode is available
-async function scanDevices() {
+// getUserMedia wrapper that checks for facing mode or device in case of mobile
+async function getNextVideoDevice() {
+
     console.log("device scan");
     try {
         let devices = await navigator.mediaDevices.enumerateDevices();
         devices.forEach(async device => {
             if (device.kind === "videoinput") {
-                console.log(device);
+                // console.log(device);
                 deviceIds.push(device.deviceId);
-
-                // ToDo: come back to this facingMode to force front/back on mobile
-                /*
-                stream = await navigator.mediaDevices.getUserMedia({video: true});
-                stream.getVideoTracks().every(track => {
-                    let settings = track.getSettings();
-                    if (settings.facingMode) {
-                        mobile = true;
-                        console.log("facingMode found");
-                        return false; //break out
-                    }
-                })
-                 */
             }
         });
     } catch (e) {
         errorHandler(e)
     }
-}
 
-// getUserMedia wrapper that checks for facing mode or device in case of mobile
-async function getMedia(init=false) {
+
+
     let constraints = {
         video: {
             width: {ideal: 1920},
@@ -64,53 +48,28 @@ async function getMedia(init=false) {
         audio: true
     };
 
-    // ToDo: this is freezing my Pixel4xl
-    /*
-    if (mobile) {
-        if (facingMode === "environment") {
-            facingMode = "user";
-        } else {
-            facingMode = "environment";
-            video.classList.remove('mirror');
-        }
+    index++;
 
-        constraints.video.facingMode = {ideal: facingMode};
+    if (index > deviceIds.length)
+        index = 0;
 
+    // If the next id happens to be the current selection, go to the next one
+    if (currentDeviceId === deviceIds[index])
+        index++;
 
-
-    } else {
-    */
-        if(!init)
-            index++;
-
-        if (index > deviceIds.length)
-            index = 0;
-
-        // If the next id happens to be the current selection, go to the next one
-        if (currentDeviceId === deviceIds[index])
-            index++;
-
-        constraints.video.deviceId = deviceIds[index];
-
-    //}
+    constraints.video.deviceId = {exact: deviceIds[index]};
 
     // How get the stream
     try {
-        console.log(constraints);
+        console.log(JSON.stringify(constraints));
 
         let stream = await navigator.mediaDevices.getUserMedia(constraints);
+        window.stream = stream; // for debugging
+
 
         let currentDeviceSettings = stream.getVideoTracks()[0].getSettings();
         currentDeviceId = currentDeviceSettings.deviceId;
-        if (currentDeviceSettings.facingMode === "environment")
-            video.classList.remove('mirror');
-        else
-            video.classList.add('mirror');
 
-
-        if (deviceIds.length <= 0) {
-            await scanDevices();
-        }
         return stream;
     } catch (e) {
         errorHandler(e)
@@ -122,37 +81,44 @@ changeCam.onclick = async () => {
 
 
     // ToDo: catch errors
-    let newStream = await getMedia().catch(err=>{
+    let newStream = await getNextVideoDevice().catch(err => {
         console.error(err);
     });
 
-    if(newStream.id === stream.id){
+    if (newStream.id === stream.id) {
         console.log("the same stream was returned, trying again");
-        newStream = await getMedia().catch(err=>console.error(err));
+        newStream = await getMedia().catch(err => console.error(err));
         //ToDo: check deviceIds for the same?
     }
 
+    stopQrScan = true;
 
-    if (extCall && extCall.open){
-        let videoSender = await pageCall.peerConnection.getSenders().find(s=> {
+    stream.getTracks().forEach(track => track.stop());
+    // newStream.getTracks().forEach(track=>stream.addTrack(track));
+    video.srcObject = newStream;
+
+    if (extCall && extCall.open) {
+        let videoSender = await pageCall.peerConnection.getSenders().find(s => {
             return s.track.kind === "video";
         });
         console.log("videoSender", videoSender);
         let newVideoTrack = newStream.getVideoTracks()[0];
-        videoSender.replaceTrack(newVideoTrack);
+        await videoSender.replaceTrack(newVideoTrack);
 
-        let audioSender = await pageCall.peerConnection.getSenders().find(s=> {
+        let audioSender = await pageCall.peerConnection.getSenders().find(s => {
             return s.track.kind === "audio";
         });
         console.log("audioSender", audioSender);
         let newAudioTrack = newStream.getAudioTracks()[0];
-        audioSender.replaceTrack(newAudioTrack);
+        await audioSender.replaceTrack(newAudioTrack);
 
         console.log("replaced preview stream track to peer");
+    } else {
+        video.onloadeddata = async () => await scanQr();
     }
 
-    stream.getTracks().forEach(track => track.stop());
     stream = newStream;
+    newStream = null;
 
 };
 
@@ -174,7 +140,6 @@ function extPeer(peerId) {
 
     peer = new Peer(`${peerId}-remote`, {debug: 0});
     let connTimeout = false;
-
 
 
     function handleDisconnect() {
@@ -209,7 +174,9 @@ function extPeer(peerId) {
             let call = peer.call(`${peerId}-ext`, stream); //, {sdpTransform: mungeH264});
             console.log("initiated preview stream call");
 
-            call.on('close', ()=>{console.log("mediaConnection ended")})
+            call.on('close', () => {
+                console.log("mediaConnection ended")
+            })
         }
     });
 
@@ -233,7 +200,6 @@ function extPeer(peerId) {
         peer.reconnect();
     });
 
-
 }
 
 
@@ -250,22 +216,22 @@ async function camPermissions() {
             })
             .catch(err => reject(err))
     });
-
-
-    // let devices = await navigator.mediaDevices.enumerateDevices();
-    // let videoDevices = devices.filter(device => device.kind === "videoinput" && device.label !== "");
-    // console.log(videoDevices);
-    // return videoDevices.length > 0
 }
 
-
+let stopQrScan = false;
 async function scanQr() {
     let canvas = document.createElement('canvas');
     //let canvas  = document.querySelector('canvas');
     let ctx = canvas.getContext("2d");
     console.log("looking for QR code");
 
+    stopQrScan = false;
     function checkQr() {
+        if(stopQrScan) {
+            console.log("stopped QR scan");
+            return;
+        }
+
         canvas.height = video.videoHeight;
         canvas.width = video.videoWidth;
         ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
@@ -274,18 +240,19 @@ async function scanQr() {
 
         if (code) {
             console.log(code.data);
-            if (code.data.toLowerCase().includes("phonecam")) {
-                let peerId = JSON.parse(code.data).phonecam;
+            if (code.data.toLowerCase().includes("webwebcam")) {
+                let peerId = JSON.parse(code.data).webwebcam;
                 if (peerId) {
                     console.log(`scanned ID: ${peerId}`);
                     extPeer(peerId);
+                    stopQrScan = true;
                 } else {
                     // This was giving violation errors
-                    requestAnimationFrame(checkQr);
                     console.log("bad QR code");
+                    requestAnimationFrame(checkQr);
                 }
             }
-        } else
+        } else{}
             requestAnimationFrame(checkQr);
     }
 
@@ -293,35 +260,50 @@ async function scanQr() {
 }
 
 const urlParams = new URLSearchParams(window.location.search);
-
-
 if (urlParams.has("id")) {
-    let peerId = urlParams.get("id");
+    peerId = urlParams.get("id");
     console.log(`Using peerid ${peerId} from URL params`);
-    // permissions.classList.add('d-none');
-    video.onloadeddata = () => extPeer(peerId);
-} else
-    video.onloadeddata = () => scanQr();
+}
+
 
 //window.addEventListener('DOMContentLoaded', ()=>{});
 
 
+
+async function startMedia(){
+    let startingConstraints = {video: {width: {ideal: 1920}, height: {ideal: 1080}}, audio: true };
+
+    stream = await navigator.mediaDevices.getUserMedia(startingConstraints);
+    video.srcObject = stream;
+
+    changeCam.classList.remove("d-none");
+
+    if(peerId){
+        video.onloadeddata = () => extPeer(peerId);
+        status.innerText = "connecting to webwebcam extension";
+    }
+    else{
+        video.onloadeddata = () => scanQr();
+        status.innerText = "looking for QR code";
+    }
+
+}
+
 camPermissions().then(async permission => {
     if (permission) {
-        stream = await getMedia(true);
-        video.srcObject = stream;
+        clickForCam.classList.add("d-none");
+        await startMedia();
+
     } else {
-        status.innerText = "click anywhere to accept media permissions";
+        status.innerText = "some temporary text"; // ToDo: remove
         console.log("Camera permissions denied; waiting for user");
         // permissions.classList.remove('d-none');
         document.onclick = async () => {
-            stream = await getMedia(true);
-            video.srcObject = stream;
-            status.classList.add('d-none');
-        }
+            await startMedia();
+        };
     }
 }).catch(err => errorHandler(err));
 
 window.addEventListener('beforeunload', () => {
-   peer.destroy();
+    peer.destroy();
 });
